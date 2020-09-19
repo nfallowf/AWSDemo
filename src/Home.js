4f23ffd3a4a8ece2c37e866ebe057824e35afa3f
Nathan Fallowfield <nfallowf@uwo.ca>
2020-09-19 12:48:37 -0400

    removed info

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Home() {
     return(
         <>
-            <h1>I'm Nate Fallowfield</h1>
-            <h3>I'm a St. Thomas, Ontario based software developer.</h3>
+            <h1>{/*}I'm Nate Fallowfield{*/}</h1>
+            <h3>{/*}I'm a St. Thomas, Ontario based software developer.{*/}</h3>
         </>
     )
 }
